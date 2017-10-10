30ca7b23c887a4344de898fab7537d302c068ec8
Matthew Wright <mattjgw@gmail.com>
2017-10-09 21:58:35 -0400

    Added functionality, loops involved

--- Dice Simulator.py
+++ Dice Simulator.py
@@ -1,8 +1,17 @@
 import random
 
-userInput = input("Hit enter to roll\n")
+again = True
 
-if userInput is "":
-    roll = random.randrange(1, 6)
-    print("You rolled a", roll)
+while again is True:
+    roll = input("Hit enter to roll\n")
+    if roll == "":
+        result = random.randrange(1, 6)
+        print("You rolled a", result, "\n")
+        answer = input("Do you want to roll again? (Y/N)\n")
+        if answer == 'N' or answer == 'n':
+            again = False
+            print("Goodbye!")
+        elif answer == 'Y' or answer == 'y':
+            again = True
+
 
