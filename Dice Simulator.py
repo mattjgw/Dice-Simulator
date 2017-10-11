555e22964f3b14eca2dcd4d7232643f924a18117
Matthew Wright <mattjgw@gmail.com>
2017-10-10 21:43:24 -0400

    Input validation loop added

--- Dice Simulator.py
+++ Dice Simulator.py
@@ -4,14 +4,19 @@ again = True
 
 while again is True:
     roll = input("Hit enter to roll\n")
-    if roll == "":
-        result = random.randrange(1, 6)
-        print("You rolled a", result, "\n")
-        answer = input("Do you want to roll again? (Y/N)\n")
+    result = random.randrange(1, 6)
+    print("You rolled a", result, "\n")
+    answer = input("Do you want to roll again? (Y/N)\n")
+    exit = 0
+    while exit == 0:
         if answer == 'N' or answer == 'n':
             again = False
             print("Goodbye!")
+            exit = 1
         elif answer == 'Y' or answer == 'y':
             again = True
+            exit = 1
+        else:
+            answer = input("Enter either Y or N")
 
 
